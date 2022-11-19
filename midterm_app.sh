591477760c68882d6f51118d6fa43a700f5a3720
qigbbravo-tip <qigbbravo@tip.edu.ph>
2022-11-19 03:56:22 +0000

    Midterm exam

---
+++ midterm_app.sh
@@ -0,0 +1,24 @@
+docker file 
+
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp design_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" > tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates" >> tempdir/Dockerfile
+echo "COPY midterm_app.py /home/myapp" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/midterm_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t midtermprojapp .
+docker run -t -d -p 5050:5050 --name midtermprojrun midtermprojapp
+docker ps -a
